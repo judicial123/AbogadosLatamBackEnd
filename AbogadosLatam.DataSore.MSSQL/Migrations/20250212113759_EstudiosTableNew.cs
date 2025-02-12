534c32b1365fa68e329bd1b9f0cb580931a9a2f6
judicial123 <69055446+judicial123@users.noreply.github.com>
2025-02-12 07:04:44 -0500

    Estudios Admin

---
+++ AbogadosLatam.DataSore.MSSQL/Migrations/20250212113759_EstudiosTableNew.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AbogadosLatam.DataSore.MSSQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class EstudiosTableNew : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estudios",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: false),
+                    LogoUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    DateModified = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudios", x => x.Id);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Pais",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateModified" },
+                values: new object[] { new DateTime(2025, 2, 12, 6, 37, 58, 908, DateTimeKind.Local).AddTicks(5100), new DateTime(2025, 2, 12, 6, 37, 58, 908, DateTimeKind.Local).AddTicks(5130) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Estudios");
+
+            migrationBuilder.UpdateData(
+                table: "Pais",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "DateCreated", "DateModified" },
+                values: new object[] { new DateTime(2025, 2, 12, 6, 35, 56, 90, DateTimeKind.Local).AddTicks(7390), new DateTime(2025, 2, 12, 6, 35, 56, 90, DateTimeKind.Local).AddTicks(7420) });
+        }
+    }
+}
